9b58433b08ef39c5b36e3d9a6db78a48aca030d7
vinoth-berkmons <vinothberkmons@gmail.com>
2016-10-29 13:54:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var person = { name:"Vinoth Berkmons", age:25, dob:"27-05-1991", pob:"Chennai"}
 
-console.log(person.name + " , " + person.age + " , " + person.dob + " , " + person.pob);
+console.log("The name is:" person.name + " , " + "Age is:"  person.age + " , " + "Date of birth is: " person.dob + " , " + "Place of Birth is: " person.pob);
